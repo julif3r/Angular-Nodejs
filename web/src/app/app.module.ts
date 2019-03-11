9225b9b0615bb9ba90d255fd2be62d8d5a02969f
Hajrullah Islami <hajrullah.islami@gmail.com>
2019-03-11 22:05:16 +0100

    Added login and home routes

--- web/src/app/app.module.ts
+++ web/src/app/app.module.ts
@@ -1,14 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { AppRoutingModule } from './app-routing.module';
+import { LoginLayoutComponent } from './components/layouts/login-layout/login-layout.component';
+import { HomeLayoutComponent } from './components/layouts/home-layout/home-layout.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    LoginLayoutComponent,
+    HomeLayoutComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
