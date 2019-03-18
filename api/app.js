20c7e90caf91c9eda27a52951928835f68fd1d01
Driton Berisha <driton@polymathlabs.co>
2019-03-18 23:34:06 +0100

    Fix docker

--- api/app.js
+++ api/app.js
@@ -1,14 +1,17 @@
+const express = require('express');
+
+const app = express();
 const http = require('http');
 
-const hostname = '127.0.0.1';
-const port = process.env.API_PORT || 3000;
+const hostname = '0.0.0.0';
+const port = 3001;
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\n');
-});
+const server = http.createServer(app);
 
-server.listen(3001, hostname, () => {
+server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
+
+app.get('/', function (req, res) {
+  res.send('GET request to the homepage')
+})
